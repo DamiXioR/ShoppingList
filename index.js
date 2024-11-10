d7219a5c1b7c285ac1b4765c6d5c5adbc4c4acb6
DamiXioR <damixiorek@damixiorek.com>
2024-11-10 17:55:09 +0100

    Improve drag and drop logic
    Added privileges for the first, penultimate and last item.
    Distinguished between new elements and those that already exist.
    Moved functionality to remove items after adding a dragged item to maintain indexing reliability.

--- index.js
+++ index.js
@@ -46,29 +46,59 @@ function dropElementOnElement(event) {
     hoveredElement.style.fontWeight = 'normal';
     hoveredElement.style.color = ''
 
-    log.debug(`Dragged Element: ${draggedElement.getAttribute('id')}`);
-    log.debug(`Hovered Element: ${hoveredElement.getAttribute('id')}`);
-
-    log.debug(`Current shopping list before any modification: ${shoppingList.map(item => item.getAttribute('id')).join(', ')}`);
-    let indexOfDraggedElement = shoppingList.indexOf(draggedElement);
-    if (shoppingList.includes(draggedElement)) {
-        shoppingList.splice(indexOfDraggedElement, 1);
-        log.debug(`Current shopping list after remove Dragged Element: ${shoppingList.map(item => item.getAttribute('id')).join(', ')}`);
-    }
-
-    let indexOfHoveredElement = shoppingList.indexOf(hoveredElement);
     draggedElement.addEventListener('drop', dropElementOnElement);
     draggedElement.addEventListener('dragenter', enterActionOnHoveredElement);
     draggedElement.addEventListener('dragleave', leaveActionOnHoveredElement);
-    log.debug(`Index of Dragged Element: ${indexOfDraggedElement}, Index of Hovered Element: ${indexOfHoveredElement}`);
-    if (indexOfHoveredElement == indexOfDraggedElement){
-        shoppingList.splice(indexOfHoveredElement + 1, 0, draggedElement);
-    } else if (indexOfHoveredElement == shoppingList.length - 1){
-        shoppingList.push(draggedElement);
+
+    log.debug(`Dragged Element: ${draggedElement.getAttribute('id')}`);
+    log.debug(`Hovered Element: ${hoveredElement.getAttribute('id')}`);
+    log.debug(`Current shopping list: ${shoppingList.map(item => item.getAttribute('id')).join(', ')}`);
+
+    let indexOfDraggedElement = shoppingList.includes(draggedElement) ? shoppingList.indexOf(draggedElement) : null;
+    let indexOfHoveredElement = shoppingList.indexOf(hoveredElement);
+
+    log.debug(`Index of Dragged Element: ${indexOfDraggedElement}, Index of Hovered Element: ${indexOfHoveredElement} before adding dragged`);
+
+    const FIRST_SL_ITEM = 0;
+    const LAST_SL_ITEM = shoppingList.length - 1;
+    const PENULTIMATE_SL_ITEM = shoppingList.length - 2;
+
+    log.debug(`Index of the last item ${LAST_SL_ITEM}`);
+    log.debug(`Index of the penultimate item ${PENULTIMATE_SL_ITEM}`);
+    log.debug(`Shopping list size ${shoppingList.length}`);
+
+    if(indexOfDraggedElement == null){
+         if (indexOfHoveredElement >= 0 && indexOfHoveredElement < PENULTIMATE_SL_ITEM){
+            log.debug(`${draggedElement.getAttribute('id')} is a new item. Place at the hovered item.`);
+            shoppingList.splice(indexOfHoveredElement, 0, draggedElement);
+         } else if (indexOfHoveredElement == PENULTIMATE_SL_ITEM){
+            log.debug(`${draggedElement.getAttribute('id')} is a new item. The hovered item is the penultimate one in the list.`);
+            shoppingList.splice(indexOfHoveredElement + 1, 0, draggedElement);
+         } else if (indexOfHoveredElement == LAST_SL_ITEM){
+            log.debug(`${draggedElement.getAttribute('id')} is a new item. Hovered item is the last on the list.`);
+            shoppingList.push(draggedElement);
+         }
     } else {
-        shoppingList.splice(indexOfHoveredElement, 0, draggedElement);
+        let draggedItemHigherFactor = indexOfDraggedElement > indexOfHoveredElement ? 1 : 0;
+        if (indexOfHoveredElement == FIRST_SL_ITEM){
+            log.debug(`${draggedElement.getAttribute('id')} exists. Hovered item is the last on the list, put dragged item first.`);
+            shoppingList.unshift(draggedElement);
+            shoppingList.splice(indexOfDraggedElement + draggedItemHigherFactor, 1);
+        } else if (indexOfHoveredElement == LAST_SL_ITEM) {
+            log.debug(`${draggedElement.getAttribute('id')} exists. Hovered item is the last on the list.`);
+            shoppingList.push(draggedElement);
+            shoppingList.splice(indexOfDraggedElement + draggedItemHigherFactor, 1);
+        } else if (draggedItemHigherFactor){
+            log.debug(`${draggedElement.getAttribute('id')} exists. Pushed on hovered element place. Dragged is higher than hovered.`);
+            shoppingList.splice(indexOfHoveredElement, 0, draggedElement);
+            shoppingList.splice(indexOfDraggedElement + draggedItemHigherFactor, 1);
+        } else {
+            log.debug(`${draggedElement.getAttribute('id')} exists. Pushed on hovered element place. Dragged is lower than hovered.`);
+            shoppingList.splice(indexOfHoveredElement + 1, 0, draggedElement);
+            shoppingList.splice(indexOfDraggedElement + draggedItemHigherFactor, 1);
+        }
     }
-    
+
     log.debug(`Current shopping list after putting Dragged Element in Hovered Element place: ${shoppingList.map(item => item.getAttribute('id')).join(', ')}`);
 
     updateToBuy();
@@ -106,9 +136,6 @@ function drop(event){
     currentlyDraggedElementId = null;
 }
 
-toBuy.addEventListener('dragover', allowDrop);
-toBuy.addEventListener('drop', drop);
-
 function startDragProduct(event){
     currentlyDraggedElementId = event.target.id;
 }
@@ -125,3 +152,6 @@ function submitProductButtonClicked(){
 }
 
 sumbitProductBtn.addEventListener('click', submitProductButtonClicked);
+
+toBuy.addEventListener('dragover', allowDrop);
+toBuy.addEventListener('drop', drop);
